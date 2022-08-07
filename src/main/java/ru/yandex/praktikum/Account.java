0ef59d1605db2df90b4c85eb91800dcae235dd7d
Igor Pershikov <adenos@mail.ru>
2022-08-07 23:20:58 +0300

    My work

--- src/main/java/ru/yandex/praktikum/Account.java
+++ src/main/java/ru/yandex/praktikum/Account.java
@@ -1,2 +1,19 @@
-package ru.yandex.praktikum;public class Account {
+package ru.yandex.praktikum;
+
+public class Account {
+
+    private final String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public boolean checkNameToEmboss() {
+        boolean length = (name.length() >= 3 && name.length() <= 19);
+        boolean spaceExist = name.indexOf(' ') != -1;
+        boolean spaceIsOne = name.indexOf(' ') == name.lastIndexOf(' ');
+        boolean spaceIsInMiddle = name.indexOf(' ') != 0 && name.indexOf(' ') != (name.length() - 1);
+
+        return length && spaceExist && spaceIsOne && spaceIsInMiddle;
+    }
 }
--- src/test/java/ru/yandex/praktikum/AccountTest.java
+++ src/test/java/ru/yandex/praktikum/AccountTest.java
@@ -1,2 +1,61 @@
-package ru.yandex.praktikum;public class AccountTest {
+package ru.yandex.praktikum;
+
+import org.junit.*;
+
+public class AccountTest {
+    @Test
+    public void correctName() {
+        Account account = new Account("Ivan Nikolaev");
+        boolean expectedResult = true;
+        boolean actualResult = account.checkNameToEmboss();
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void nameWithoutSpace() {
+        Account account = new Account("IvanNikolaev");
+        boolean expectedResult = false;
+        boolean actualResult = account.checkNameToEmboss();
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void nameWithStartSpace() {
+        Account account = new Account(" Ivan Nikolaev");
+        boolean expectedResult = false;
+        boolean actualResult = account.checkNameToEmboss();
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void nameWithEndSpace() {
+        Account account = new Account("Ivan Nikolaev ");
+        boolean expectedResult = false;
+        boolean actualResult = account.checkNameToEmboss();
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void nameWithTwoSpaces() {
+        Account account = new Account("Ivan  Nikolaev");
+        boolean expectedResult = false;
+        boolean actualResult = account.checkNameToEmboss();
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void nameLengthLessThree() {
+        Account account = new Account("Ni");
+        boolean expectedResult = false;
+        boolean actualResult = account.checkNameToEmboss();
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void nameLengthMore19() {
+        Account account = new Account("Konstantin Stanislavskii");
+        boolean expectedResult = false;
+        boolean actualResult = account.checkNameToEmboss();
+        Assert.assertEquals(actualResult, expectedResult);
+    }
 }
